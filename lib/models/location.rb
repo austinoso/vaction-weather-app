ef7d630b82dddab243a2f61a4654143f623b3208
austinoso <osorio_austin@yahoo.com>
2020-02-12 09:45:30 -0800

    Refactored location geration code

--- lib/models/location.rb
+++ lib/models/location.rb
@@ -3,7 +3,7 @@ class Location < ActiveRecord::Base
     has_many :users, through: :user_locations
 
     def self.search
-        country = ISO3166::Country.new(ISO3166::Country.all.sample.alpha2)
+        country = ISO3166::Country.all.sample
         city = country.cities[country.cities.keys.sample]
         location = Location.create(name: city.name, country: country.name, latitude: city.latitude, longitude: city.longitude)
     end
