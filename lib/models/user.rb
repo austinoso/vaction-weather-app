8186d67eba1d3b33d841c37395b2cc867a652b7b
austinoso <osorio_austin@yahoo.com>
2020-02-12 13:54:38 -0800

    Granted user ability to set :max_humidity

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
     has_many :user_locations
     has_many :locations, through: :user_locations
+
+    def set_max_humidity(humidity_percent)
+        if humidity_percent > 100
+            puts "Can't set higher than 100%"
+            puts "Setting your max humidity to 100%"
+            self.max_humidity = 100
+        elsif humidity_percent < 40
+            puts "Can't set lower than 40%"
+            puts "Setting your max humidity to 40%"
+            self.max_humidity = 40
+        else
+            self.max_humidity = humidity_percent
+        end
+    end
 end
