01a370dfc82cd04987d3b26686eb68e0a4e91e4a
austinoso <osorio_austin@yahoo.com>
2020-02-11 16:48:53 -0800

    migrated files

--- db/migrate/003_create_user_locations.rb
+++ db/migrate/003_create_user_locations.rb
@@ -2,7 +2,7 @@ class CreateUserLocations < ActiveRecord::Migration[5.2]
     def change
         create_table :user_locations do |t|
             t.string :name
-            t.sting :country
+            t.string :country
             t.integer :user_id
             t.integer :location_id
         end
--- db/migrate/004_create_weathers.rb
+++ db/migrate/004_create_weathers.rb
@@ -1,9 +1,9 @@
 class CreateWeathers < ActiveRecord::Migration[5.2]
     def change 
-        create table :weathers do |t|
+        create_table :weathers do |t|
             t.string :location
-            t.float: temperature 
-            t.float: humidity
+            t.float :temperature 
+            t.float :humidity
             t.integer :location_id
         end
     end
