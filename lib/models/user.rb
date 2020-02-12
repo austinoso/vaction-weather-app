b6a1915db118eb2e8db19ea8876380bcd6d92784
austinoso <osorio_austin@yahoo.com>
2020-02-12 14:09:35 -0800

    Granted Users to set :temp_pref

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -4,15 +4,27 @@ class User < ActiveRecord::Base
 
     def set_max_humidity(humidity_percent)
         if humidity_percent > 100
-            puts "Can't set higher than 100%"
-            puts "Setting your max humidity to 100%"
+            puts "Can't set higher than 100%."
+            puts "Setting your max humidity to 100%."
             self.max_humidity = 100
-        elsif humidity_percent < 40
-            puts "Can't set lower than 40%"
-            puts "Setting your max humidity to 40%"
-            self.max_humidity = 40
+        elsif humidity_percent < 60
+            puts "Can't set lower than 60%."
+            puts "Setting your max humidity to 60%."
+            self.max_humidity = 60
         else
             self.max_humidity = humidity_percent
         end
     end
+
+    def set_temp_pref(temp)
+        if temp == "cold"
+            self.temp_pref = temp
+        elsif temp == "hot"
+            self.temp_pref = temp
+        else
+            puts "You didn't enter a vaild temperature."
+            puts 'Please enter either "cold" or "hot" or leave blank for no preference.'
+        end
+    end
+
 end
