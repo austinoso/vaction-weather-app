d8657e876500da42a51d0aa2bb63550fba2097ba
SCDan0624 <dmayle1@hotmail.com>
2020-02-12 17:14:49 -0800

    created a few weather methods, will need to plug in coordinates

--- lib/models/location.rb
+++ lib/models/location.rb
@@ -17,7 +17,7 @@ class Location < ActiveRecord::Base
     def current_temp(location)
         puts "Enter city that you would like to see the temperature of."
         results = gets.chomp
-        weather_data = RestClient.get("https://api.darksky.net/forecast/773a54e5e33804db37622417cee9961e/#{coordinates}")
+        weather_data = RestClient.get("https://api.darksky.net/forecast/773a54e5e33804db37622417cee9961e/42.3601,-71.0589")
         response_hash = JSON.parse(weather_data)
         puts "Current temp is: #{response_hash["currently"]["temperature"] + 32.round}F"
     end
